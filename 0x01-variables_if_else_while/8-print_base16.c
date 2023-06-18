fd97b579ab8c8347da87250c090eec18989e9ca6
Najat <craazylolla@gmail.com>
2023-06-18 13:33:15 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: using the main function
+* this program prints the alphabets
+* Return: 0
+*/
+int main(void)
+{
+	int n = 0;
+	char ch = 'a';
+
+	while (n <= 9)
+	{
+		putchar(digit + 48);
+		a++;
+	}
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
