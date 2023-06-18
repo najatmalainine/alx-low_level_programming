99f701fd88c6c782e72c3baf8e113daf576f2920
Najat <craazylolla@gmail.com>
2023-06-18 11:46:31 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: using the main function
+* this program prints the alphabets
+* Return: 0
+*/
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+
+	return (0);
+}
+
