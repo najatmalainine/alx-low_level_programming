ba033aa6a307e6782080b07d8a1479866ccce2c1
Najat <craazylolla@gmail.com>
2023-06-18 12:44:43 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: using the main function
+* this program prints numbers
+* Return: 0
+*/
+int main(void)
+{
+	int digit;
+
+	while (digit < 9)
+	{
+		putchar(digit + 48);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
