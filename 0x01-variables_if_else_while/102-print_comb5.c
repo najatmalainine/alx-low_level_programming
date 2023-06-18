ebe8cc865197d344bad24152aa79207e02b7d961
Najat <craazylolla@gmail.com>
2023-06-18 15:56:26 -0700

    m

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: using the main function
+* this program prints the alphabets
+* Return: 0
+*/
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = i; k <= 9; k++)
+			{
+				for (l = j + 1; l <= 9; l++)
+				{
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(' ');
+					putchar(k + 48);
+					putchar(l + 48);
+					if (!((i == 9 && j == 8) && (k == 9 && l == 9)))
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
