5a7256f0d23592741eb0bdc28b987e5297add1ae
Najat <craazylolla@gmail.com>
2023-06-18 11:17:57 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: using the main function
+* this program prints the alphabets
+* Return: 0
+*/
+int main(void)
+{
+	char alph = 'a';
+	char ch = 'A';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+		while (ch <= 'Z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
