1ae0cfcb3044b9aa2294285319a58097da03625d
Najat <craazylolla@gmail.com>
2023-06-20 13:42:36 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print alphabet - prints alphabets
+*
+* Description: using the main function
+* this program prints the alphabets
+* Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
+
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,10 @@
 
 int _putchar(char c);
 
+/**
+ * void print_alphabet(void): prints the alphabet, in lowercase
+ */
+
+void print_alphabet(void);
+
 #endif
