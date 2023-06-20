7171306dcd148e4a67ba93a22c96af0fe5802369
Najat <craazylolla@gmail.com>
2023-06-20 15:24:38 -0700

    m

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
+#include "main.h"
+
+/**
+* _islower returns 1 if lowercase, 0 if not
+* Description: using the main function
+* this program prints the alphabets
+* Return: 0
+*/
 
 int _islower(int c)
 {
