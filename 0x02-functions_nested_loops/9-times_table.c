ee995e6605dc247f6c19ca588d41a303e1017506
Najat <craazylolla@gmail.com>
2023-06-21 13:50:53 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			z = i * j;
-			_putchar("%d", z);
+			printf("%d", z);
 			if (j != 9)
 			{
 			_putchar(',');
@@ -23,7 +23,7 @@ void times_table(void)
 			}
 
 		}
-		_putchar("\n");
+		_putchar('\n');
 	}
 
 }
