e20ef8f6f86950937924ee1d85957f6723843d45
Najat <craazylolla@gmail.com>
2023-06-21 16:17:44 -0700

    m

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,11 @@ void print_times_table(int n)
 	if (n <= 15 && n >= 0)
 	{
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= n; i++)
 	{
 		_putchar('0');
 
-		for (j = 1; j < 10; j++)
+		for (j = 1; j <= n; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
