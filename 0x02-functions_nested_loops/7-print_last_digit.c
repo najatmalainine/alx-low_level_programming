be8f3a1be25e548219c512f9396acfe00c5e7ee3
Najat <craazylolla@gmail.com>
2023-06-21 11:55:00 -0700

    m

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,14 @@
 
 int print_last_digit(int n)
 {
+	int last_dig;
 
 	if (n < 0)
-		n *= -1;
+		last_dig = -1 * (n % 10);
+	else
+		last_dig = n % 10;
 
-	_putchar('0' + (n % 10));
+	_putchar(last_dig + '0');
 
-	return (n % 10);
+	return (last_dig);
 }
