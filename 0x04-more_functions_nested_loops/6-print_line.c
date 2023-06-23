9d2429f743f43be942cd9d270c48da9e1fe16734
Najat <craazylolla@gmail.com>
2023-06-23 14:01:15 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints '_' n times
+ * @n: number to check
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int j;
+
+
+	if (n > 0)
+	{
+		for (j = 1; j <= n; j++)
+		{
+
+			putchar('_');
+
+		}
+		putchar('\n');
+	}
+	else
+		putchar('\n');
+}
