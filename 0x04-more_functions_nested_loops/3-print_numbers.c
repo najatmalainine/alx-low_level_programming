934a6b587fbb103cd93a72543f34624199cba07e
Najat <craazylolla@gmail.com>
2023-06-23 06:52:30 -0700

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers
+*
+* Description: using the main function
+* this program prints numbers
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+
+}
