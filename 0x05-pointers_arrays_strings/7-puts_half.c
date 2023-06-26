3dc99ccef75cf771dddfd74b463deaed1c731b2e
Najat <craazylolla@gmail.com>
2023-06-26 12:56:17 -0700

    m

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,26 +10,26 @@ void puts_half(char *str)
 {
 
 	int length = 0;
-
+	int i, halfLength, n;
 	/* Calculate the length of the string*/
 	while (str[length] != '\0')
 	{
 		length++;
 	}
 
-	int halfLength = length / 2;
-	int n = (length - 1) / 2;
+	halfLength = length / 2;
+	n = (length - 1) / 2;
 
 	/* Print the second half of the string*/
 	if (length % 2 == 0)
-		for (int i = halfLength; i < length; i++)
+		for (i = halfLength; i < length; i++)
 		{
 			_putchar(str[i]);
 
 		}
 
 	else if (length % 2 != 0)
-		for (int i = n; i < length; i++)
+		for (i = length - n; i < length; i++)
 		{
 			_putchar(str[i]);
 
