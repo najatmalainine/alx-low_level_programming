935927909459480fa60738af733f8bc14113e18a
Najat <craazylolla@gmail.com>
2023-06-26 06:23:19 -0700

    m

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+
+void reset_to_98(int *n);
+
+#endif
