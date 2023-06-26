51ae588740f72fb5b0b764e07a51127a4ea584e9
Najat <craazylolla@gmail.com>
2023-06-26 12:12:27 -0700

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,24 @@
 #include "main"
 
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		++count;
+
+	return (count);
+}
+
+
 /**
  * puts2 - prints every second character of a string, followed by a new line
  * @str: string to print
@@ -9,11 +28,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; ++i)
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 
-	}
+		_putchar(str[i]);
+
+
 	_putchar('\n');
 }
