4252c4917e51071d695b31f0f43a7a00e3e4a4b7
Najat <craazylolla@gmail.com>
2023-06-29 13:42:25 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,12 @@ char *_strcat(char *dest, char *src)
 {
 
 	int i = 0;
-
+	int j = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	int j = 0;
 
 	while (src[j] != '\0')
 	{
@@ -27,5 +26,6 @@ char *_strcat(char *dest, char *src)
 	}
 
 	dest[i] = '\0';
+
 	return (dest);
 }
