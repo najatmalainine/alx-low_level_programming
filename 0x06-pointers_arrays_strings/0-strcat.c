da58e83360cb10299f34b409fb9a355e98d700f3
Najat <craazylolla@gmail.com>
2023-06-29 13:12:02 -0700

    m

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	int j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+}
