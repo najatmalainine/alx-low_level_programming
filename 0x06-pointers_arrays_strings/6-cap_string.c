5c3a346f15ab5d66a10ccb92ee4625c38b1ca925
Najat <craazylolla@gmail.com>
2023-06-30 13:53:25 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,45 +1,32 @@
 #include "main.h"
-#include <ctype.h>
+
 
 /**
  * cap_string - capitalize first letter of each word
  * @s: string to manipulate
  * Return: string
  */
-
 char *cap_string(char *s)
 {
-	 /* Flag to indicate whether the next character should be capitalized*/
+	int i = 0;
 
-	int capitalize = 1;
+	/* check first index for capital */
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
 
-	for (int i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0') /* iterate through string */
 	{
-		/* Check if the current character is a separator*/
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' ||
-			s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' ||
-			s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
-		{
-			capitalize = 1; /* Set the flag to capitalize the next character*/
-		}
-		/* Check if the current character is an alphabet (a-z, A-Z)*/
-		else if (isalpha(s[i]))
-		{
-			/* Capitalize the current character if the flag is set*/
-			if (capitalize)
-			{
-				s[i] = toupper(s[i]);
-				capitalize = 0; /* Reset the flag*/
-			}
-			else
-			{
-				s[i] = tolower(s[i]); /* Convert to lowercase if the flag is not set*/
-			}
-		}
-		else
-		{
-			capitalize = 0; /* Reset the flag for non-alphabet characters*/
-		}
+
+		/* if lowercase and prior char is separator, capitalize*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
+		i++;
 	}
 
 	return (s);
