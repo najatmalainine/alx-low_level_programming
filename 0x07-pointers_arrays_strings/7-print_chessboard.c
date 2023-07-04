cbb4d618f352d9ca363b7d10d4b124b5c931b392
Najat <craazylolla@gmail.com>
2023-07-04 12:20:02 -0700

    m

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_chessboard - print chessboard given set 2D array
+ * @a: 2D array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
