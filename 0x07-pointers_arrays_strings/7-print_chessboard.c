f101b4d2355ea8bcb84ccd3e9bd8635d49c70b13
Najat <craazylolla@gmail.com>
2023-07-04 12:22:48 -0700

    m

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,9 +10,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (int i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			printf("%c ", a[i][j]);
 		}
