9ada5faa6d065ea40391c9a4b487581559a382ae
Najat <craazylolla@gmail.com>
2023-07-06 16:28:47 -0700

    m

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * wildcmp - compare if string with wildcard mattches
+ * @s1: one string
+ * @s2: one string
+ * Return: 1 if matched, 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0') && wildcmp(s1 + 1, s2));
+
+	return (0);
+}
