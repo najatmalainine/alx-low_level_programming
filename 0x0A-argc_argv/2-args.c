c655d06aa70f364aae982e212ebe790e4c0ae9fd
Najat <craazylolla@gmail.com>
2023-07-07 14:47:10 -0700

    m

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char const *argv[])
+{
+	for (i = 0; i = argc - 1; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
