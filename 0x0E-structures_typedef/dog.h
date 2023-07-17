6131acf3e2a278f5cd8fdb743a8039ccc7358f96
Najat <craazylolla@gmail.com>
2023-07-17 12:48:48 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,6 @@
  * @owner: third member
  */
 
-typedef struct dog dog_t;
 
 struct dog
 {
@@ -16,6 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
