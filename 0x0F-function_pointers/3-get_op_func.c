89c304f0e9dc58f8517ef3278aeaadf2c3a0bc04
Najat <craazylolla@gmail.com>
2023-07-19 16:32:17 -0700

    m

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,13 +19,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	i = 0;
+
 
 	while (ops[i].op != NULL)
 	{
-		if (s == ops[i].op)
+		if (*s == *ops[i].op)
 			return (ops[i].f);
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (!f)
+	if (f == NULL || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
