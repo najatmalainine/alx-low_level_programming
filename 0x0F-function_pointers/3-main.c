aa1a4fab9b775c63c16a238450a4b5e761b02434
Najat <craazylolla@gmail.com>
2023-07-19 16:22:39 -0700

    m

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,13 +29,13 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (f == NULL || (argv[2][1] != '\0'))
+	if (!f)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
