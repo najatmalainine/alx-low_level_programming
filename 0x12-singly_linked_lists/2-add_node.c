71d91ffec8d7a3b8b95e88760a289d437706692c
Najat <craazylolla@gmail.com>
2023-07-28 16:04:56 -0700

    m

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,27 +1,44 @@
 #include "lists.h"
 
 /**
+ * _strlen - find string length
+ * @str: string
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
  * add_node - add node to beginning of linked list
  * @head: linked list
  * @str: data for new node
  * Return: address of new element, or NULL if failed
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
+	if (!new_node || !head)
 		return (NULL);
-
-	new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
+	if (str)
 	{
-		free(new_node);
-		return (NULL);
+
+		new_node->str = strdup(str);
+
+		if (new_node->str == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
 	}
 	new_node->next = *head;
 
