1e3cee9caf7591c17e489bb7b0914c6e66b501c0
Najat <craazylolla@gmail.com>
2023-07-29 13:46:30 -0700

    m

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,11 @@
-section			.text
-		extern	printf
-		golbal	main
-main:
-		mov				edi, msg
-		mov				eax, 0
-		call	printf
+section .data
+    msg db 'Hello, Holberton', 0xa, 0
 
-section
-		msg	db	'Hello, Holberton', 0xa, 0
+section .text
+    extern printf
+    global main
+
+main:
+    mov edi, msg
+    xor eax, eax
+    call printf
