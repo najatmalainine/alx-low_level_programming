de806d3ecaa7ac50f84949e8f15f5b829200d763
Najat <craazylolla@gmail.com>
2023-08-07 15:43:50 -0700

    m

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int writebytes;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	writebytes = write(fd, text_content, _strlen(text_content));
+	if (writebytes == -1 || writebytes != _strlen(text_content))
+		return (-1);
+	close(fd);
+	return (1);
+}
