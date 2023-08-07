b1809b1e6dca84462ae0516e78a7850cf7768a73
Najat <craazylolla@gmail.com>
2023-08-07 16:47:55 -0700

    m

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * main - copies one file to another
+ * @argc: should be 3 (./a.out copyfromfile copytofile)
+ * @argv: first is file to copy from (fd_1), second is file to copy to (fd_2)
+ * Return: 0 (success), 97-100 (exit value errors)
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, readbytes, writebytes;
+	char buffer[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+	while ((readbytes = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		if (readbytes == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			close(file_from);
+			close(file_to);
+			exit(98);
+		writebytes = write(file_to, buffer, readbytes);
+		if (writebytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+	}
+		if (close(file_from) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+			close(file_to);
+			exit(100);
+		}
+
+		if (close(file_to) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+			exit(100);
+		}
+		exit(0);
+}
