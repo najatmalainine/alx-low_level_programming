db4bbea01a2d57bc229672a6b1539a7a277166fd
Najat <craazylolla@gmail.com>
2023-09-14 15:55:05 -0700

    m

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - get sum of data in doubly linked list
+ * @head: pointer to head of doubly linked list
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
