c9869a834d2bf11512c9db286ed91302b21f4795
Najat <craazylolla@gmail.com>
2023-09-14 13:58:40 -0700

    m

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints all data of doubly linked list
+ * @h: pointer to head of list
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
